cbdca69b40fe704a251c5e4e6174394ae293ad08
Babafemi-Adigun, Aby <babafev@stthom.edu>
2025-03-20 14:49:16 -0500

    1st push

---
+++ main.py
@@ -0,0 +1,18 @@
+class Library:
+  def __init__(self, name, books, users):
+    self.name = name
+    self.books = books
+    self.users = users
+
+class Book:
+  def __init__(self, title, author, genre, available):
+    self.title = title
+    self.author = author
+    self.genre = genre
+    self.available = available
+
+class User:
+  def __init__(self, memberID, name):
+    self.memberID = memberID
+    self.name = name
+
